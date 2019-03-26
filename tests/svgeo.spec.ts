c66d994e8bc216a6dfcffa43047b7b11c5e93b38
LiamAttClarke <liam.att.clarke@gmail.com>
2019-03-26 00:08:25 -0400

    Added unit tests

--- tests/svgeo.spec.ts
+++ tests/svgeo.spec.ts
@@ -5,13 +5,21 @@ import * as chaiRoughly from "chai-roughly";
 import * as fs from "fs";
 import * as path from "path";
 import { mercator } from "projections";
+import * as svgson from "svgson";
 import * as GeoJSONValidation from "geojson-validation";
 import * as svgeo from "../src/svgeo";
 import Vector2 from "../src/Vector2";
 
 chai.use(chaiRoughly);
 
-const TOLERANCE = 1e-6;
+const defaultConvertOptions:svgeo.ConvertSVGOptions = {
+  scale: 1,
+  center: {
+    longitude: 0,
+    latitude: 0
+  },
+  subdivideThreshold: 10
+};
 
 describe("svgeo", () => {
 
@@ -79,37 +87,25 @@ describe("svgeo", () => {
     const svgMeta:svgeo.SVGMetaData = { width: 512, height: 512 };
 
     it("should return the point projected with a mercator projection", () => {
-      const convertOptions:svgeo.ConvertSVGOptions = {
-        center: { longitude: 0, latitude: 0 },
-        scale: 1
-      };
-      const coord1 = svgeo.svgPointToCoordinate(new Vector2(256, 256), svgMeta, convertOptions);
+      const coord1 = svgeo.svgPointToCoordinate(new Vector2(256, 256), svgMeta, defaultConvertOptions);
       const expectedCoord1 = mercator({ x: 0.5, y: 0.5 });
       expect(coord1).to.deep.equal([expectedCoord1.lon, expectedCoord1.lat]);
-      const coord2 = svgeo.svgPointToCoordinate(new Vector2(384, 128), svgMeta, convertOptions);
+      const coord2 = svgeo.svgPointToCoordinate(new Vector2(384, 128), svgMeta, defaultConvertOptions);
       const expectedCoord2 = mercator({ x: 0.75, y: 0.25 });
       expect(coord2).to.deep.equal([expectedCoord2.lon, expectedCoord2.lat]);
     });
 
     it("should clamp latitudes that exceed (+/-)85 degrees", () => {
-      const convertOptions:svgeo.ConvertSVGOptions = {
-        center: { longitude: 0, latitude: 0 },
-        scale: 1
-      };
-      const coord1 = svgeo.svgPointToCoordinate(new Vector2(256, 512), svgMeta, convertOptions);
+      const coord1 = svgeo.svgPointToCoordinate(new Vector2(256, 512), svgMeta, defaultConvertOptions);
       expect(Math.round(coord1[1])).to.equal(-85);
-      const coord2 = svgeo.svgPointToCoordinate(new Vector2(256, 0), svgMeta, convertOptions);
+      const coord2 = svgeo.svgPointToCoordinate(new Vector2(256, 0), svgMeta, defaultConvertOptions);
       expect(Math.round(coord2[1])).to.equal(85);
     });
 
     it("should clamp longitudes that exceed (+/-)180 degrees", () => {
-      const convertOptions:svgeo.ConvertSVGOptions = {
-        center: { longitude: 0, latitude: 0 },
-        scale: 1
-      };
-      const coord1 = svgeo.svgPointToCoordinate(new Vector2(600, 256), svgMeta, convertOptions);
+      const coord1 = svgeo.svgPointToCoordinate(new Vector2(600, 256), svgMeta, defaultConvertOptions);
       expect(coord1[0]).to.equal(180);
-      const coord2 = svgeo.svgPointToCoordinate(new Vector2(-600, 256), svgMeta, convertOptions);
+      const coord2 = svgeo.svgPointToCoordinate(new Vector2(-600, 256), svgMeta, defaultConvertOptions);
       expect(coord2[0]).to.equal(-180);
     });
 
@@ -143,18 +139,183 @@ describe("svgeo", () => {
 
   });
 
-  describe("lineTransformer", () => {});
-  describe("rectTransformer", () => {});
-  describe("polylineTransformer", () => {});
-  describe("polygonTransformer", () => {});
-  describe("ellipseTransformer", () => {});
-  describe("pathTransformer", () => {});
-  describe("groupTransformer", () => {});
+  describe("lineTransformer", () => {
+    const svgLine = fs.readFileSync(path.join(__dirname, "files/line.svg"), "utf8");
+
+    it("should convert a SVG Line to a GeoJSON LineString", async () => {
+      const parsedSVG = await svgson.parse(svgLine, { camelcase: true });
+      const svgMeta:svgeo.SVGMetaData = {
+        width: parseFloat(parsedSVG.attributes.width),
+        height: parseFloat(parsedSVG.attributes.height)
+      };
+      const features = svgeo.lineTransformer(parsedSVG.children[0], svgMeta, defaultConvertOptions);
+      expect(features.length).to.equal(1);
+      expect(GeoJSONValidation.isFeature(features[0])).to.be.true;
+      expect(GeoJSONValidation.isLineString(features[0].geometry)).to.be.true;
+      expect(features[0].geometry.coordinates).to.deep.equal([[-90, 66.27715161480374], [90, -66.74715120228447]]);
+    });
+
+  });
+
+  describe("rectTransformer", () => {
+    const svgRect = fs.readFileSync(path.join(__dirname, "files/rect.svg"), "utf8");
+
+    it("should convert a SVG Rect to a GeoJSON Polygon", async () => {
+      const parsedSVG = await svgson.parse(svgRect, { camelcase: true });
+      const svgMeta:svgeo.SVGMetaData = {
+        width: parseFloat(parsedSVG.attributes.width),
+        height: parseFloat(parsedSVG.attributes.height)
+      };
+      const features = svgeo.rectTransformer(parsedSVG.children[0], svgMeta, defaultConvertOptions);
+      expect(features.length).to.equal(1);
+      expect(GeoJSONValidation.isFeature(features[0])).to.be.true;
+      expect(GeoJSONValidation.isPolygon(features[0].geometry)).to.be.true;
+      expect(features[0].geometry.coordinates).to.deep.equal([[
+        [-90, 66.27715161480374],
+        [90, 66.27715161480374],
+        [90, -66.74715120228447],
+        [-90, -66.74715120228447],
+        [-90, 66.27715161480374]
+      ]]);
+    });
+
+  });
+
+  describe("polylineTransformer", () => {
+    const svgPolyline = fs.readFileSync(path.join(__dirname, "files/polyline.svg"), "utf8");
+
+    it("should convert a SVG Polyline to a GeoJSON LineString", async () => {
+      const parsedSVG = await svgson.parse(svgPolyline, { camelcase: true });
+      const svgMeta:svgeo.SVGMetaData = {
+        width: parseFloat(parsedSVG.attributes.width),
+        height: parseFloat(parsedSVG.attributes.height)
+      };
+      const features = svgeo.polylineTransformer(parsedSVG.children[0], svgMeta, defaultConvertOptions);
+      expect(features.length).to.equal(1);
+      expect(GeoJSONValidation.isFeature(features[0])).to.be.true;
+      expect(GeoJSONValidation.isLineString(features[0].geometry)).to.be.true;
+      expect(features[0].geometry.coordinates).to.deep.equal([
+        [-72, -18.27182881120864],
+        [-63, -34.329566962935075],
+        [-53.999999999999986, -26.581366777928018],
+        [-45, -47.81958166243998],
+        [-35.99999999999999, -41.423544535420085],
+        [-26.999999999999986, -58.53541562856473],
+        [-18.000000000000014, -53.51464959455888],
+        [-9.000000000000007, -66.74715120228447],
+        [0, -62.9275917419856]
+      ]);
+    });
+
+  });
+
+  describe("polygonTransformer", () => {
+    const svgPolygon = fs.readFileSync(path.join(__dirname, "files/polygon.svg"), "utf8");
+
+    it("should convert a SVG Polygon to a GeoJSON Polygon", async () => {
+      const parsedSVG = await svgson.parse(svgPolygon, { camelcase: true });
+      const svgMeta:svgeo.SVGMetaData = {
+        width: parseFloat(parsedSVG.attributes.width),
+        height: parseFloat(parsedSVG.attributes.height)
+      };
+      const features = svgeo.polygonTransformer(parsedSVG.children[0], svgMeta, defaultConvertOptions);
+      expect(features.length).to.equal(1);
+      expect(GeoJSONValidation.isFeature(features[0])).to.be.true;
+      expect(GeoJSONValidation.isPolygon(features[0].geometry)).to.be.true;
+      expect(features[0].geometry.coordinates).to.deep.equal([[
+        [-90, -72.9069533245733],
+        [-80.99999999999999, -80.8324317832456],
+        [-53.999999999999986, -80.8324317832456],
+        [-72, -83.29731570170968],
+        [-63, -85.10173601678947],
+        [-90, -84.2698837069131],
+        [-116.99999999999999, -85.10173601678947],
+        [-108, -83.29731570170968],
+        [-126, -80.8324317832456],
+        [-99.00000000000001, -80.8324317832456],
+        [-90, -72.9069533245733]
+      ]]);
+    });
+
+  });
+
+  describe("ellipseTransformer", () => {
+    const svgCircle = fs.readFileSync(path.join(__dirname, "files/circle.svg"), "utf8");
+    const svgEllipse = fs.readFileSync(path.join(__dirname, "files/ellipse.svg"), "utf8");
+
+    it("should convert a SVG Circle to a GeoJSON Polygon", async () => {
+      const parsedSVG = await svgson.parse(svgCircle, { camelcase: true });
+      const svgMeta:svgeo.SVGMetaData = {
+        width: parseFloat(parsedSVG.attributes.width),
+        height: parseFloat(parsedSVG.attributes.height)
+      };
+      const features = svgeo.ellipseTransformer(parsedSVG.children[0], svgMeta, defaultConvertOptions);
+      expect(features.length).to.equal(1);
+      expect(GeoJSONValidation.isFeature(features[0])).to.be.true;
+      expect(GeoJSONValidation.isPolygon(features[0].geometry)).to.be.true;
+      expect(features[0].geometry.coordinates).to.deep.equal([[
+        [-148.359375, 77.49063100012613],
+        [-149.42981907406002, 76.26998288998547],
+        [-152.47818588956417, 75.1442488285283],
+        [-157.04038923236592, 74.34226982668066],
+        [-162.421875, 74.05075451479573],
+        [-167.80336076763408, 74.34226982668066],
+        [-172.36556411043583, 75.1442488285283],
+        [-175.41393092593998, 76.26998288998547],
+        [-176.48437500000003, 77.49063100012613],
+        [-175.41393092593998, 78.60434071192854],
+        [-172.36556411043583, 79.47143860215122],
+        [-167.80336076763408, 80.01412765638977],
+        [-162.421875, 80.19805080143433],
+        [-157.04038923236592, 80.01412765638977],
+        [-152.47818588956417, 79.47143860215122],
+        [-149.42981907406005, 78.60434071192854],
+        [-148.359375, 77.49063100012613]
+      ]]);
+    });
+
+    it("should convert a SVG Ellipse to a GeoJSON Polygon", async () => {
+      const parsedSVG = await svgson.parse(svgEllipse, { camelcase: true });
+      const svgMeta:svgeo.SVGMetaData = {
+        width: parseFloat(parsedSVG.attributes.width),
+        height: parseFloat(parsedSVG.attributes.height)
+      };
+      const features = svgeo.ellipseTransformer(parsedSVG.children[0], svgMeta, defaultConvertOptions);
+      expect(features.length).to.equal(1);
+      expect(GeoJSONValidation.isFeature(features[0])).to.be.true;
+      expect(GeoJSONValidation.isPolygon(features[0].geometry)).to.be.true;
+      expect(features[0].geometry.coordinates).to.deep.equal([[
+        [-113.20312499999999, 77.49063100012613],
+        [-113.27083978117223, 77.41577320923906],
+        [-113.47333199432956, 77.34120200300026],
+        [-114.27356907406003, 77.19586011023722],
+        [-117.32193588956417, 76.94062403087021],
+        [-127.265625, 76.70590373615683],
+        [-141.328125, 77.49063100012613],
+        [-141.2604102188278, 77.56505055287148],
+        [-141.05791800567044, 77.6383238747957],
+        [-140.25768092594, 77.77872154588538],
+        [-137.20931411043583, 78.01782803039414],
+        [-127.265625, 78.22973324675914],
+        [-113.20312499999999, 77.49063100012613]
+      ]]);
+    });
+
+  });
+
+  describe("pathTransformer", () => {
+
+
+
+  });
+
+  describe("groupTransformer", () => {
+
+  });
 
   describe("convertSVG", () => {
-
-    const svg_shapes = fs.readFileSync(path.join(__dirname, "files/test_shapes.svg"), "utf8");
-    const svg_noDimensions = fs.readFileSync(path.join(__dirname, "files/test_no-dimensions.svg"), "utf8");
+    const svg_shapes = fs.readFileSync(path.join(__dirname, "files/shapes.svg"), "utf8");
+    const svg_noDimensions = fs.readFileSync(path.join(__dirname, "files/no-dimensions.svg"), "utf8");
 
     it("should convert an SVG string to valid GeoJSON", async () => {
       const geoJSON = await svgeo.convertSVG(svg_shapes);
@@ -191,11 +352,6 @@ describe("svgeo", () => {
           expect(error.message).to.equal("SVG is missing width and height attributes.");
         });
     });
-
-    // TODO: test options.center
-    // TODO: test options.scale
-    // TODO: test options.subdivideThreshold
-    // TODO: test SVGs with width and heights of various units
 
   });
 
