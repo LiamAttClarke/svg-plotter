79a6766993eaf27d28f6d3e95919672c08f911e1
LiamAttClarke <liam.att.clarke@gmail.com>
2021-02-14 20:59:33 -0500

    Adding missing dist files

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,7 +2,7 @@ import { INode } from 'svgson';
 import { ConvertSVGOptions, SVGMetaData } from './types';
 export declare function getSVGMetadata(parsedSVG: INode): SVGMetaData;
 export interface ConvertSVGOutput {
-    geojson: GeoJSON.GeoJSON;
+    geojson: GeoJSON.FeatureCollection;
     errors: string[];
 }
 export declare function convertSVG(input: string, options?: ConvertSVGOptions): ConvertSVGOutput;
